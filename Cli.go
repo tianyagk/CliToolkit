77ee89aa51a998eb8f7d9aa52c9da953080486e2
Aeoluze <270352204@qq.com>
2021-10-16 17:16:39 +0800

    Update Cli.go

--- Cli.go
+++ Cli.go
@@ -1,3 +1,16 @@
+// Copyright @ 2021 github.com/tianyagk
+// Licensed under the GNU GENERAL PUBLIC LICENSE (the "License");
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// CliToolkit is a small cmd package for Go cmd shell interaction application
+// inspired by go and cobra
+
 package CliToolkit
 
 import (
